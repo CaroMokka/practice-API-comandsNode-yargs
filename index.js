993fa3becf4ea16f7fed3beca78503945ca8f9b4
carolina <ci.arayagonzalez@gmail.com>
2024-10-20 23:55:01 -0300

    add-a command is created to list a data file

--- index.js
+++ index.js
@@ -1,48 +1,97 @@
-import yargs from "yargs"
-import { hideBin } from "yargs/helpers"
-import axios from "axios"
-import { promises as fs } from "fs"
-import { fileURLToPath } from "url"
-import { dirname } from "path"
-import clc from "cli-color"
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import axios from "axios";
+import { promises as fs } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import clc from "cli-color";
 
-const URL_BASE = "https://pokeapi.co/api/v2/"
+const URL_BASE = "https://pokeapi.co/api/v2/";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+class Pokemon {
+  constructor(id, name, height, weight, experience, type) {
+    (this.poke_id = id),
+      (this.poke_name = name),
+      (this.poke_height = height),
+      (this.poke_weight = weight),
+      (this.poke_experience = experience),
+      (this.poke_type = type);
+  }
+}
 
 yargs(hideBin(process.argv))
-    .command(
-        "registrar",
-        "Comando para registrar datos de un pokemón",
-        {
-            id: {
-                alias: "r",
-                describe: "Identificador único para registrar un pokemón",
-                demandOption: true,
-                type: "number"
-            }
-        },
-        async ({ id }) => {
+  .command(
+    "registrar",
+    "Comando para registrar datos de un pokemón",
+    {
+      id: {
+        alias: "r",
+        describe: "Identificador único para registrar un pokemón",
+        demandOption: true,
+        type: "number",
+      },
+    },
+    async ({ id }) => {
+      try {
+        const { data } = await axios.get(`${URL_BASE}/pokemon/${id}`);
 
-            try {
-                const { data } = await axios.get(`${URL_BASE}/pokemon/${id}`)
+        const contentTxt = await fs.readFile(
+          `${__dirname}/files/pokemons.txt`,
+          "utf-8"
+        );
+        const contentJS = JSON.parse(contentTxt);
 
-                const contentTxt = await fs.readFile(`${__dirname}/files/pokemons.txt`, "utf-8")
-                const contentJS = JSON.parse(contentTxt)
+        const busqueda = contentJS.find((item) => item.id === data.id);
+        if (busqueda) return console.log(clc.red("Pokemón ya registrado"));
 
-                const busqueda = contentJS.find( (item) => item.id === data.id )
-                if(busqueda) return console.log(clc.red("Pokemón ya registrado"))
+        contentJS.push(data);
 
-                contentJS.push(data)
+        fs.writeFile(
+          `${__dirname}/files/pokemons.txt`,
+          JSON.stringify(contentJS),
+          "utf-8"
+        );
+        console.log(clc.green("Pokemón registrado con éxito"));
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  )
+  .command(
+    "listar",
+    "Comando para listar pokemones registrados",
+    {},
+    async () => {
+      try {
+        const contentTxt = await fs.readFile(
+          `${__dirname}/files/pokemons.txt`,
+          "utf-8"
+        );
+        const contentJS = JSON.parse(contentTxt);
 
-                fs.writeFile(`${__dirname}/files/pokemons.txt`, JSON.stringify(contentJS), "utf-8")
-                console.log(clc.green("Pokemón registrado con éxito"))
-            } catch(err) {
-                console.log(err)
-            }
-        }
-    )
-.help().argv
+        const listPokemons = contentJS.map((item) => {
+          return new Pokemon(
+            item.id,
+            item.name,
+            item.height,
+            item.weight,
+            item.base_experience,
+            item.types[0].type.name
+          );
+        });
 
-//Requerimiento de pokemón ==> name, height, weight
+        console.log(clc.bgGreen("Lista actualizada"), listPokemons);
+        fs.writeFile(
+          `${__dirname}/files/list-pokemons.txt`,
+          JSON.stringify(listPokemons),
+          "utf-8"
+        );
+      } catch (err) {
+        console.error(clc.red("Error al procesar los archivos"), err);
+      }
+    }
+  )
+  .help().argv;
